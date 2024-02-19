6dcc2055e2f472744abb32f71e8cfec5ecae3b95
tpaing00 <tpaing00@mylangara.ca>
2024-02-19 00:55:09 -0800

    feat: test CICD

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.use('/api/v1', router)
 app.get("/", (req, res, next) => {
    res.json({
         status:200,
-        message: "Hello world!!!"
+        message: "Hello world!!! CICD is working fine!!!"
    })
 });
 
