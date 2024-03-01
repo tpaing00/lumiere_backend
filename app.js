413f9c3b8570dce36ea97bf13d47d0ed48eaad55
tpaing00 <tpaing00@mylangara.ca>
2024-03-01 15:29:17 -0800

    feat: remove greenlock

--- app.js
+++ app.js
@@ -3,9 +3,6 @@ const cors = require('cors');
 const app = express();
 const bodyParser = require('body-parser');
 const router = require('./routes'); //same as ('./routes/index')
-const greenlockExpress = require('greenlock-express');
-const http = require('http');
-const https = require('https');
 
 require('./models/db');
 require('./models/Firebase');
@@ -15,26 +12,10 @@ app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/api/v1', router)
 
-// Set up HTTP server to redirect to HTTPS
-http.createServer(greenlockExpress.init({
-    packageRoot: __dirname,
-    configDir: './greenlock.d',
-    maintainerEmail: 'tinzarpaing@gmail.com',
-    cluster: false
-  })).listen(80);
-  
-  // Set up HTTPS server
-  https.createServer(greenlockExpress.init({
-    packageRoot: __dirname,
-    configDir: './greenlock.d',
-    maintainerEmail: 'tinzarpaing@gmail.com',
-    cluster: false
-  })).listen(443);
-
 app.get("/", (req, res, next) => {
    res.json({
         status:200,
-        message: "Hello world!!! CI/CD is working fine!!! ENV file added via docker script. Now need to fix domain name and SSL!"
+        message: "Hello world!!! CI/CD is working fine!!! ENV file added via docker script. Domain and SSL is working fine too!"
    })
 });
 
@@ -47,5 +28,3 @@ app.get("/*", (req, res, next) => {
 app.listen(8080, () => {
     console.log("Server running on port 8080.");
 });
-
-// module.exports = app;
