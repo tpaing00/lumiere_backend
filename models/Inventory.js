886bb1e172d6d30074b3ff0ea215ab0ce1f34b2b
greeshma017 <gdharmapalan00@mylangara.ca>
2024-03-15 12:36:39 -0700

    feat: P2S-262 update invetory model for initial stock.

--- models/Inventory.js
+++ models/Inventory.js
@@ -4,12 +4,21 @@ const Schema = mongoose.Schema;
 const InventorySchema = new Schema({
     userId:{type: String, required:true, maxLength: 30, default:"sampleUserId"},
     barcodeNumber:{type: String, required:true, maxLength: 30},
-    stockQuantity:{type: Number, default:0},
+    stockQuantity:{type: Number, default:0, set: setInitialStock},
+    initialStock: { type: Number, default: 0 },
     totalValue: { type: Number, required: true },
     dateAdded:{type : Date, default: Date.now },
     addToInventory:{type: String, enum: ["Internal Use", "Retail"]},
     expiryDate: {type : Date, required:true}
 });
+
+// Define setter
+function setInitialStock(stock) {
+    if (!this.initialQuantity) {
+        this.initialQuantity = stock;
+    }
+    return stock;
+}
 
 const Inventory = mongoose.model('Inventory', InventorySchema);
 
