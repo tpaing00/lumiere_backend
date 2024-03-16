3cfd1d931499b131491097026e5e949dde93cc57
greeshma017 <gdharmapalan00@mylangara.ca>
2024-03-16 00:43:21 -0700

    feat: P2S-264 update getSale

--- routes/sale.js
+++ routes/sale.js
@@ -4,6 +4,7 @@ const router = express.Router({mergeParams:true});
 const saleCtrl= require('../controllers/saleController');
 
 router.get("/sale", saleCtrl.getSale);
+router.get("/sale/:id", saleCtrl.getSale);
 router.get("/totalsale", saleCtrl.getTotalSale);
 router.get("/soldbycategory", saleCtrl.getsoldQuantityByCategory)
 router.get("/topbyproductname", saleCtrl.getTopSoldQuantitiesByProductName)
